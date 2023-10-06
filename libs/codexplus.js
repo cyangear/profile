b6574b8e1b96bd68a98d1353ecb1d5149d201fe5
qubikl <132519950+cyangear@users.noreply.github.com>
2023-10-06 12:59:12 -0500

    Create codexplus.js

---
+++ libs/codexplus.js
@@ -0,0 +1,25 @@
+(async function(){
+  var libstring = {}
+  var libdata = {}
+  var libuser = {}
+  var libmath = {}
+  var libvideo = {}
+  var libaudio = {}
+  var liblogic = {}
+  var libinput = {}
+  var errors = {}
+  var classes = {}
+
+  codexplus = Object.freeze(Object.assign(this, {
+    string: Object.freeze(libstring),
+    data: Object.freeze(libdata),
+    user: Object.freeze(libuser),
+    math: Object.freeze(libmath),
+    video: Object.freeze(libvideo),
+    audio: Object.freeze(libaudio),
+    logic: Object.freeze(liblogic),
+    input: Object.freeze(libinput),
+    errors: Object.freeze(errors),
+    classes: Object.freeze(classes)
+  }))
+}).call({})
